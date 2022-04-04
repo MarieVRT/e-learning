daf1b302f4d4b181b2af010688c36a9d317e1b32
MarieVRT <57631852+MarieVRT@users.noreply.github.com>
2022-04-04 15:28:49 +0200

    add authentification

---
+++ resources/views/layouts/template.blade.php
@@ -0,0 +1,70 @@
+<head>
+    <title> E-learning </title>
+    <meta charset="UTF-8">
+    <meta name="description" content="pictures, laravel">
+    <meta name="keywords" content="pictures, laravel">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://templates.iqonic.design/metordash/laravel/public/storage/4/carsystem.png" rel="shortcut icon">
+
+    <!-- Bootstrap CSS -->
+    <!-- <link rel="stylesheet" href="https://templates.iqonic.design/metordash/laravel/public/assets/css/bootstrap.min.css"/> -->
+    <link rel="stylesheet" href="https://templates.iqonic.design/metordash/laravel/public/css/app.css"/>
+    <!-- Typography CSS -->
+    <link rel="stylesheet" href="https://templates.iqonic.design/metordash/laravel/public/assets/css/typography.css"/>
+    <!-- Style CSS -->
+    <link rel="stylesheet" href="https://templates.iqonic.design/metordash/laravel/public/assets/css/style.css"/>
+    <link rel="stylesheet" href="https://templates.iqonic.design/metordash/laravel/public/assets/css/custom.css"/>
+    <!-- Responsive CSS -->
+    <link rel="stylesheet" href="https://templates.iqonic.design/metordash/laravel/public/assets/css/responsive.css"/>
+    <!-- <link href="https://templates.iqonic.design/metordash/laravel/public/plugin/dataTables/jquery.dataTables.min.css" rel="stylesheet"> -->
+    <link href="https://templates.iqonic.design/metordash/laravel/public/plugin/dataTables/css/dataTables.bootstrap4.min.css" rel="stylesheet">
+    <link rel='stylesheet' href="https://templates.iqonic.design/metordash/laravel/public/assets/css/slick.css" />
+    <link rel='stylesheet' href="https://templates.iqonic.design/metordash/laravel/public/assets/css/slick-theme.css" />
+</head>
+
+
+<div>
+    <section class="sign-in-page">
+        <div class="container p-0">
+            <div class="row no-gutters">
+                <div class="col-sm-12 align-self-center">
+                    <div class="sign-in-from bg-white">
+                    @yield('content')
+                </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+
+<script src="https://templates.iqonic.design/metordash/laravel/public/js/app.js"></script>
+<!-- Appear JavaScript -->
+<script src="https://templates.iqonic.design/metordash/laravel/public/assets/js/jquery.appear.js"></script>
+<!-- Countdown JavaScript -->
+<script src="https://templates.iqonic.design/metordash/laravel/public/assets/js/countdown.min.js"></script>
+<!-- Wow JavaScript -->
+<script src="https://templates.iqonic.design/metordash/laravel/public/assets/js/wow.min.js"></script>
+<!-- Apexcharts JavaScript -->
+<!-- Slick JavaScript -->
+<script src="https://templates.iqonic.design/metordash/laravel/public/assets/js/slick.min.js"></script>
+<!-- Select2 JavaScript -->
+<script src="https://templates.iqonic.design/metordash/laravel/public/assets/js/select2.min.js"></script>
+<!-- Smooth Scrollbar JavaScript -->
+<script src="https://templates.iqonic.design/metordash/laravel/public/assets/js/smooth-scrollbar.js "></script>
+<!-- lottie JavaScript -->
+<script src="https://templates.iqonic.design/metordash/laravel/public/assets/js/lottie.js "></script>
+<script src="https://templates.iqonic.design/metordash/laravel/public/assets/js/core.js"></script>
+<script src="https://templates.iqonic.design/metordash/laravel/public/assets/js/charts.js"></script>
+<script src="https://templates.iqonic.design/metordash/laravel/public/assets/js/animated.js"></script>
+<!-- High Chart JavaScript -->
+<!-- morris chart JavaScript -->
+<!-- Chart Custom JavaScript -->
+<script src="https://templates.iqonic.design/metordash/laravel/public/assets/js/chart-custom.js "></script>
+
+
+
+
+<!-- Custom JavaScript -->
+<script src="https://templates.iqonic.design/metordash/laravel/public/assets/js/custom.js"></script>
